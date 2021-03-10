75ddcae2c512b244d352dbaf037d76595ad54927
Olivia Faraone <oliviafaraone1@MacBook-Pro-10.local>
2021-03-10 00:08:58 -0700

    hide password

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const connection = mysql.createConnection({
     // Your MySQL username
     user: 'root',
     // Your MySQL password
-    password: 'Fockey226!',
+    password: '****!',
     database: 'employees'
   });
   
